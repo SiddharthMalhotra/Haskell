4e70d292fa5a644c3918f0e0c8389761cf98fd1f
SiddharthMalhotra <codemalhotra@gmail.com>
2018-04-05 00:00:12 +1000

    removing old 3
    putting new 3

--- Haskell/99 Haskell Problems/3-Kthelementofalist.hs
+++ Haskell/99 Haskell Problems/3-Kthelementofalist.hs
@@ -1,6 +1,6 @@
 elementAt :: [Int] -> Int -> Int 
 
 elementAt [] _ = error "Empty list"
-elementAt (x:xs) s = count xs n
-where 
- 
+elementAt (x:xs) s = check xs 1e
+	where 
+		check [] _ = error "Empty" 
