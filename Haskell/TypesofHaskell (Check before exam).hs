40b6c87b7bf77a42fb76977a8b3b718cf3b9f14d
SiddharthMalhotra <codemalhotra@gmail.com>
2018-04-06 03:21:43 +1000

    Adding more types
    Adding more types

--- Haskell/TypesofHaskell (Check before exam).hs
+++ Haskell/TypesofHaskell (Check before exam).hs
@@ -1,2 +1,4 @@
 :t (<)													(<) :: Ord a => a -> a -> Bool
 :t map (+3) 											map (+3) :: Num b => [b] -> [b]
+:t foldl 												foldl :: Foldable t => (b -> a -> b) -> b -> t a -> b
+:t foldr												foldr :: Foldable t => (a -> b -> b) -> b -> t a -> b
