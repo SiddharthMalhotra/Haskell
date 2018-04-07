b66795a5470e19a330581df53772a0dfd0fd5e52
SiddharthMalhotra <codemalhotra@gmail.com>
2018-04-07 12:47:58 +1000

    Alternate palindrome
    Alternate palindrome

---
+++ Haskell/99 Haskell Problems/6-Palindrome-alternate.hs
@@ -0,0 +1,16 @@
+import Data.Char
+import Data.List
+
+stringToLower :: String -> String
+stringToLower str = map toLower str
+
+stringletters :: String -> String
+stringletters str = filter isAlpha str
+
+stringLowerLetters :: String -> String
+stringLowerLetters = stringToLower.stringletters
+
+isPalindrome :: String -> Bool
+isPalindrome str = s1 == reverse(s1)
+  where s1 = map toLower . filter isAlpha $ str
+
--- Haskell/TypesofHaskell (Check before exam).hs
+++ Haskell/TypesofHaskell (Check before exam).hs
@@ -1,5 +1,10 @@
 :t (<)													(<) :: Ord a => a -> a -> Bool
+
 :t map (+3) 											map (+3) :: Num b => [b] -> [b]
+
 :t foldl 												foldl :: Foldable t => (b -> a -> b) -> b -> t a -> b
+
 :t foldr												foldr :: Foldable t => (a -> b -> b) -> b -> t a -> b
+
 :t filter (not.(==3)) [1,2,3]							      :: (Num a, Eq a) => [a]
+
