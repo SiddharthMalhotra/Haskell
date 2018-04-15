b52c0e634a329039bfb21af8ad169622502748ec
SiddharthMalhotra <codemalhotra@gmail.com>
2018-04-15 18:07:39 +1000

    new types

--- Haskell/TypesofHaskell (Check before exam).hs
+++ Haskell/TypesofHaskell (Check before exam).hs
@@ -8,3 +8,20 @@
 
 :t filter (not.(==3)) [1,2,3]							      :: (Num a, Eq a) => [a]
 
+:t "abc" ++ "def" 										"abc" ++ "def" :: [Char]
+
+-- A point to remember Very important!
+{- 
+		When representing Char you simply write Char or [Char]
+		But When representing Int you will give function :: Num a => Int or [Int]
+
+		Note that you need to tell what is the type of variable first and then whether it is singleton number alone or a list
+		So type => list (For Interger)
+-}
+
+
+:t (+)  												(+) :: Num a => a -> a -> a  \
+
+:t [1, 2, 3]											[1,2,3] :: Num a => [a]
+
+:t [1,2,3.0]											[1,2,3.0] :: Fractional a => [a]
